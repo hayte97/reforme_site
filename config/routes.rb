3ed82ec1d86aa5d14de4f3c619648fdc9cdb7f83
hayte97 <s.hayate0907@gmail.com>
2020-04-14 23:59:33 +0900

    possible_location途中経過

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'home#top' # トップページ
   
-  get 'top'     => 'home#top'      # トップページ
-  get 'company' => 'home#company'  # 会社案内ページ
-  get 'service' => 'home#service'  # 事業内容ページ
-  get 'contact' => 'home#service'  # お問い合わせページ
+  get 'top'      => 'home#top'      # トップページ
+  get 'company'  => 'home#company'  # 会社案内ページ
+  get 'possible' => 'home#possible'  # 事業内容ページ
+  get 'contact'  => 'home#contact'  # お問い合わせページ
 end
