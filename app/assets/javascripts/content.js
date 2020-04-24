e333171671325f9551d11307a4aa15887c67da8d
kazuyuki_mera <mera.kazuyuki@techbase.jp>
2020-04-24 23:40:25 +0900

    ヘッダー修正

--- app/assets/javascripts/content.js
+++ app/assets/javascripts/content.js
@@ -3,18 +3,9 @@ $(function () {
   let menus = $('.menu-title');
   menusAry = Array.prototype.slice.call(menus);
 
-  // メニュー名を取得
-  let menu1 = menusAry[0].text;
-  let menu2 = menusAry[1].text;
-
   // コンテンツ情報を取得
   let contents = $('.content-info');
   contentsAry = Array.prototype.slice.call(contents);
-
-  // コンテンツのidを取得
-  let content1 = contentsAry[0]
-  let content2 = contentsAry[1];
-
 
   // メニューの中でクリックイベントが発生した場所を探し、下で定義したtabSwitch関数を呼び出す
   menusAry.forEach(function (value) {
@@ -23,6 +14,14 @@ $(function () {
 
   // クラスの切り替えをtabSwitch関数で定義
   function menuSwitch() {
+    // メニュー名を取得
+    let menu1 = menusAry[0].text;
+    let menu2 = menusAry[1].text;
+
+    // コンテンツのidを取得
+    let content1 = contentsAry[0]
+    let content2 = contentsAry[1];
+    
     // 全てのactiveクラスのうち、最初の要素を削除
     $(".active")[0].classList.remove("active");
 
